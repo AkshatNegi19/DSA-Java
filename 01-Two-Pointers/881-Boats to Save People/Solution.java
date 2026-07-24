86c118ff98fa62fd4fe295484636d9d582a51352
AkshatNegi19 <negiakt05@gmail.com>
2026-07-24 12:29:31 +0530

    Create Solution.java

---
+++ 01-Two-Pointers/881-Boats to Save People/Solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int left = 0;
+        int right = people.length-1;
+        int res = 0;
+
+        while (left<=right) {
+            int sum = people[left] + people[right];
+            if (sum<=limit) {
+                res++;
+                left++;
+                right--;
+            } else {
+                res++;
+                right--;
+            }
+        };
+
+        return res;
+    }
+}
